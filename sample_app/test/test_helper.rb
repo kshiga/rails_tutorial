7814f1b1644d68f36e2bc4455e2b5c5f6f89ddb3
Kaitlyn Higa <kaitlyn@nationbuilder.com>
2016-03-29 15:58:58 -0700

    Updated Gem versions

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -3,8 +3,8 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 # not loading for some reason  but included in the tutorial - which is requesting to use an IDE: dont know how this affects
 #   the minitest gem
-# require "minitest/reporters"
-# Minitest::Reporters.use!
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
