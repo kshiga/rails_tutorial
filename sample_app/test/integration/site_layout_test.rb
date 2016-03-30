a42037b62fb5314dc1b1db7876e4f8bfa88474ef
Kaitlyn Higa <kaitlyn@nationbuilder.com>
2016-03-30 14:28:33 -0700

    Chapter 9 exercises

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
   test "layout links" do
     get root_path
@@ -10,6 +13,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    assert_select "a[href=?]", login_path
+
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", edit_user_path(id: @user.id)
+    assert_select "a[href=?]", user_path(id: @user.id)
+
+
     get signup_path
     assert_select "title", full_title("Sign up")
   end
